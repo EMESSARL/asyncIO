cd46b270873a5e12bc8f8082a854b00b9a4466e0
Emery Kouassi Assogba <assogba.emery@gmail.com>
2021-08-15 15:34:02 +0100

    meilleur gestion du buffer sans les memset

--- test_io.c
+++ test_io.c
@@ -10,6 +10,7 @@
 #include <memory.h>
 #include <sys/wait.h>
 
+#define BUFFSIZE 131072
 //#define false 0;
 
 int main(int argc, char *argv[]){
@@ -17,9 +18,10 @@ int main(int argc, char *argv[]){
    int pfd[2]; //pour le pipe
    pid_t pid;
    fd_set readset, writeset;
-   char buff[255];
-   char buffer[BUFSIZ];
-   int ret, ret_sel,fd_max, timeout = 0, bytestosend;
+   char buff[BUFFSIZE];
+   char buffer[BUFFSIZE];
+   int ret, ret_sel,fd_max, timeout = 0, bytestosend = 0;
+   int offset = 0, bytesrecved = 0;
    int should_write = 0;
    FD_ZERO(&readset);
    FD_ZERO(&writeset);
@@ -33,32 +35,21 @@ int main(int argc, char *argv[]){
    fcntl(0, F_SETFL, O_NONBLOCK );
    fcntl(1, F_SETFL, O_NONBLOCK );
    
-   memset(buffer, 0, BUFSIZ);
+   memset(buffer, 0, BUFFSIZE);
    
-   while ((c = getopt(argc, argv, "t")) != -1)
-   {
+   while ((c = getopt(argc, argv, "t")) != -1){
       switch (c)
       {
          case 't':
-            //fprintf(stderr,"Je rcois un param ici\n");
             with_time = true;
-            //fprintf(stderr,"Je l'ai changé\n");
             break;
          default:
             break;
       }
    }
-
-
-
-     // close(pfd[0]); //fermer la lecture du pipe
-
-      /*Le père va écrire dans le pipe. On vérifie que le descripteur d'écriture est donc prêt*/
-                  /*Création du pipe*/
-
-   if ((pipe(pfd) == -1))
-   {
-      //printf("Erreur lors de la création du pipe\n");
+   
+   if ((pipe(pfd) == -1)){
+      fprintf(stderr, "Erreur lors de la création du pipe\n");
       return 1;
    }
    
@@ -66,39 +57,30 @@ int main(int argc, char *argv[]){
    fcntl(pfd[1], F_SETFL, O_NONBLOCK);
 
    /*Créer un processus fils pour lire dans notre pipe*/
-   if ((pid = fork()) == -1)
-   {
-      //printf("Echec de la création du processus fils\n");
+   if ((pid = fork()) == -1){
+      fprintf(stderr, "Echec de la création du processus fils\n");
       return 1;
    }
 
-   if (pid == 0)
-   {
-      // FD_SET(1, &writeset);
-      // FD_SET(pfd[1], &writeset);
+   if (pid == 0){
       long bytesrecv = 0;
       long byteswrite = 0;
       fd_max = 0;
       close(pfd[1]); //fermer l'extrémité de lecture pour éviter des comportements bizarres
 
-      while (1)
-      {
-         //FD_SET(0, &readset);
+      while (1){
+         FD_SET(1, &writeset);
+         fd_max = 1;
          FD_SET(pfd[0], &readset);
          if (pfd[0] > fd_max)
-         {
             fd_max = pfd[0];
-         }
-         
-       
-         ret_sel = select(fd_max+1, &readset, NULL, NULL, &out_time);
+         ret_sel = select(fd_max+1, &readset, &writeset, NULL, &out_time);
 
-         if (ret_sel < 0)
-         {
+         if (ret_sel < 0){
             fprintf(stderr, "Erreur du select dans le fils\n");
+            return 1;
          }
-         else if(ret_sel == 0)
-         {
+         else if(ret_sel == 0){
             fprintf(stderr, "Je suius fatigué d'attendre dans le fils bytesrecv (%ld) byteswrite(%ld)\n", bytesrecv, byteswrite);
             timeout++;
             if(timeout >= 3)
@@ -108,31 +90,45 @@ int main(int argc, char *argv[]){
          else
          {
             timeout = 0;
-            if (FD_ISSET(pfd[0], &readset))
-            {
-               int n = read(pfd[0],buffer, BUFSIZ);
-               if(n < 0){
-                  fprintf(stderr, "On a un petit problème lors du read dans le fils\n");
+            if (FD_ISSET(pfd[0], &readset)){
+               if(!bytesrecved){
+                  bytesrecved = read(pfd[0],buffer, BUFFSIZE);
+                  if(bytesrecved < 0){
+                     fprintf(stderr, "On a un petit problème lors du read dans le fils\n");
+                  }
+                  else if (bytesrecved == 0){
+                     close(pfd[0]);
+                     fprintf(stderr, "end of child\n");
+                     break;
+                  }
+                  else{
+                     bytesrecv +=bytesrecved;
+                     should_write = 1;
+                     offset = 0;
+                  }
                }
-               if (n == 0)
-               {
-                  close(pfd[0]);
-                  fprintf(stderr, "end of child\n");
-                  break;
-               }
-               if(n>0){
-                  bytesrecv +=n;
-                  int m = printf("%s", buffer, n);
-                  if(m > 0)
-                    byteswrite +=m;
+               //memset(buffer, 0, BUFFSIZE);
+            }
+            if(FD_ISSET(1, &writeset)){
+               if(should_write){
+                  int m = write(1, buffer+offset, bytesrecved);
+                  if(m < 0)
+                     fprintf(stderr, "un problème dans le fils %s", strerror(errno));
+                  else {  
+                     byteswrite +=m;
+                     offset +=m;
+                     bytesrecved -= m;
+                     if(!bytesrecved){
+                        should_write = 0;
+                        offset = 0;
+                     } 
+                  }
                   
                }
-               memset(buffer, 0, BUFSIZ);
-            } 
-               
+            }                
          }
-            FD_CLR(pfd[0], &readset);
-            //FD_CLR(pfd[1], &writeset);      
+         FD_CLR(pfd[0], &readset);
+         FD_CLR(1, &writeset);      
          
       }//Fin while du select dans le fils
       close(pfd[0]);
@@ -148,72 +144,61 @@ int main(int argc, char *argv[]){
         FD_SET(0, &readset);
         FD_SET(pfd[1], &writeset);
         if (pfd[1] > fd_max)
-        {
            fd_max = pfd[1];
-        }
+        
         if (with_time)
-        {
-            //fprintf(stderr, "je suis dans le if\n");
            ret_sel = select(fd_max+1, &readset, &writeset, NULL, &out_time);
-        }
-         else
-         {
-            //fprintf(stderr,"Je suis dansle else plutot\n");
-            ret_sel = select(fd_max+1, &readset, &writeset, NULL, NULL);
-         }
-
-         if (ret_sel < 0)
-         {
+        else
+           ret_sel = select(fd_max+1, &readset, &writeset, NULL, NULL);
+        
+        if (ret_sel < 0){
             fprintf(stderr,"Une erreur est survenue! dans le select du père\n");
-         }
-         else if (ret_sel == 0)
-         {
+            break;
+        }
+        else if (ret_sel == 0){
             fprintf(stderr,"Temps expiré, rien à lire, rien à écrire dans le père!\n");
             break;
-         }
-         else
-         {
-            if(FD_ISSET(0, &readset)){
-               //fprintf(stderr, "Quelque chose à lire\n");
-               
-               if(buffer[0] == '\0'){
-                   ret = read(0, buffer, 255);
-               if(ret < 0){
-                  fprintf(stderr, "Il ya eu un problème lors de la lecture\n");
-               }
-               if(ret>0)
-               {
-                  should_write = 1;
-                  bytestosend = ret;
-                  bytesread +=ret;
-                 
-               }
-               else if(ret == 0)
-               {
+        }
+        else{
+           if(FD_ISSET(0, &readset)){
+              if(!bytestosend){
+                 ret = read(0, buffer, BUFFSIZE);
+                 if(ret < 0)
+                    fprintf(stderr, "Il ya eu un problème lors de la lecture\n");
+                 else if(ret>0){
+                    should_write = 1;
+                    bytestosend += ret;
+                    bytesread +=ret;
+                 }
+                 else {
                   fprintf(stderr, "end of father\n");
                   break;
-               }
-               }
+                 }
+              }
+               
             }
 
             if (FD_ISSET(pfd[1], &writeset))
             {
                
                //scanf("%s", buffer);
-               if (should_write)
-               {
-                  //printf("Yo les gars entrez des données pour notre pipe!\n");
-                  ret = write(pfd[1], buffer, bytestosend);
+               if (should_write){
+                  ret = write(pfd[1], buffer+offset, bytestosend);
                   if(ret < 0){
-                     fprintf(stderr, "il ya eu un problème lors de l'écriture\n");
+                     fprintf(stderr, "il ya eu un problème lors de l'écriture %d\n", offset);
                      //break;
                   }
-                  if(ret < bytestosend){
-                     fprintf(stderr, "on a pas pu tout envoyé\n");
-                  }else
+                  bytessent += ret;
+                  bytestosend -= ret; 
+                  offset +=  ret;
+                  
+                  if(bytestosend){
+                     fprintf(stderr, "on a pas pu tout envoyer %d\n", bytestosend);
+                  }else{
                     should_write = 0;
-                  memset(buffer, 0, BUFSIZ);
-                  bytessent += ret;                 
+                    offset = 0;
+                  //memset(buffer, 0, BUFFSIZE); 
+                  }              
                }
             }
             
